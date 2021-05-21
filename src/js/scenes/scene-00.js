637935e47ba10c5bc38d49dc88789013f19858b6
Billy Z Duke <billyzduke@gmail.com>
2021-05-21 11:26:16 -0700

    sweet looking rocket flares

--- src/js/scenes/scene-00.js
+++ src/js/scenes/scene-00.js
@@ -1,4 +1,4 @@
-import { TimelineMax as TL } from 'gsap'
+import { gsap, TimelineMax as TL } from 'gsap'
 
 import g from '../glob'
 import { setAddOn } from '../utils'
@@ -22,6 +22,7 @@ const setScene00 = (c, n) => {
 
   g.tL.yore.to('#tpTitleScreen', {
     duration: 2,
+    onComplete: spinTime,
     opacity: 0,
   })
     .set('#tpTitles', {
@@ -31,5 +32,41 @@ const setScene00 = (c, n) => {
   return true
 }
 
+const spinTime = () => {
+  gsap.set('#deLorean', {
+    rotateX: 60,
+    rotateY: 200,
+    rotateZ: 180,
+    scale: 1.23,
+  })
+  gsap.to('#deLorean', {
+    duration: 20,
+    ease: 'none',
+    repeat: -1,
+    rotateX: 30,
+    yoyo: true,
+  })
+  gsap.to('#deLorean', {
+    duration: 40,
+    ease: 'none',
+    repeat: -1,
+    rotateY: 240,
+    yoyo: true,
+  })
+  gsap.to('.lightBar > div', {
+    duration: 1.5,
+    ease: 'power1.in',
+    opacity: 0.12,
+    repeat: -1,
+    yoyo: true,
+  })
+  // gsap.to('#deLorean', {
+  //   duration: 30,
+  //   ease: 'none',
+  //   repeat: -1,
+  //   rotateZ: 360,
+  //   yoyo: true,
+  // })
+}
 
 export { scene00, setScene00 }
