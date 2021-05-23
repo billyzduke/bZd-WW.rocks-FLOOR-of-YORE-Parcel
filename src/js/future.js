d25befaf6368a05e57e3e4da3fd0e2caddd6a56b
Billy Z Duke <billyzduke@gmail.com>
2021-05-22 17:57:38 -0700

    well, it all works except for the delorean moving at like 2 frames per second. Which means I likely will have to rebuild it another way. Now to find one that actually fucking works.

--- src/js/future.js
+++ src/js/future.js
@@ -1,14 +1,123 @@
-import { gsap } from 'gsap'
+import { gsap, TimelineMax as TL } from 'gsap'
 import { TextPlugin } from 'gsap/TextPlugin'
 
+import g from './glob'
 import { setFlux } from './flux'
-import { setAddOn } from './utils'
+import { gsapTick, randOnum, setAddOn, toggleFermata } from './utils'
 
 gsap.registerPlugin(TextPlugin)
 
 const setFuture = () => {
+  g.tL.bttf = new TL({ defaults: { overwrite: 'auto' } })
+  g.qss.wormHoler = gsap.quickSetter('#blindingFlash', 'css')
   setFlux()
   gsap.set('#future', { opacity: 1 })
+}
+
+const wormHoleHexes = [
+  'f8f8f8',
+  'fff7f7',
+  'f7efef',
+  'efe7f5',
+  'eadaee',
+  'e8ccee',
+  'd7bbed',
+  'eef7fb',
+  'a5adf7',
+  'd8bcf8',
+  '877bae',
+  'ffffff'
+]
+
+const wormHoleFlashTick = () => {
+  const o = randOnum(1, 100)
+  const c = randOnum(0, 11)
+  g.qss.wormHoler({
+    opacity: o < 56 ? 0 : o,
+    backgroundColor: wormHoleHexes[c]
+  })
+}
+
+const beginFuture = () => {
+  const blindingFlashUnTick = gsapTick(wormHoleFlashTick)
+
+  setTimeout(() => {
+    g.el.deLorean.style.opacity = 1
+    setTimeout(() => {
+      toggleFermata(true, { exceptTLs: ['bttf'] })
+      blindingFlashUnTick()
+      flyOver()
+    }, 700)
+  }, 2300)
+}
+
+const flyOver = () => {
+
+  gsap.to('.lightBar > div', {
+    duration: 1.5,
+    ease: 'power1.in',
+    opacity: 0.12,
+    repeat: -1,
+    yoyo: true,
+  })
+
+  g.tL.bttf.set('#flux', {
+      opacity: 0
+    })
+  .to('#deLorean', {
+    duration: 9,
+    ease: 'power2.in',
+    translateZ: -500,
+  })
+  .to('#deLorean', {
+    duration: 7.5,
+    ease: 'power2.in',
+    rotateY: '-=90',
+  }, '<5')
+  .to('#deLorean', {
+    duration: 3.5,
+    ease: 'power2.in',
+    rotateZ: '+=30',
+    repeat: 1,
+    yoyo: true,
+  }, '<2')
+  .to('#deLorean', {
+    duration: 5,
+    ease: 'power2.in',
+    translateX: '-=50',
+    translateY: '-=200',
+  }, '<')
+  .to('#deLorean', {
+    duration: 3.5,
+    ease: 'power2.in',
+    rotateX: '-=15',
+    repeat: 1,
+    yoyo: true,
+  }, '>')
+  .to('#deLorean', {
+    duration: 4,
+    ease: 'power2.out',
+    translateX: '+=150',
+    translateY: '-=100',
+    translateZ: -400,
+  }, '<')
+  // CONVERT FROM FLYING TO DRIVING MODE
+  // TURN OFF WHEEL ROCKETS
+  .to('#deLorean #wheels .wheel .rocket', {
+    duration: 1.25,
+    scaleY: 0,
+  }, '>')
+  // SET WHEELS IN WELLS
+  .to('#wheelsLeft', {
+    duration: 4,
+    rotateY: 90,
+    translateZ: '-=42',
+  }, '>')
+  .to('#wheelsRight', {
+    duration: 4,
+    rotateY: -90,
+    translateZ: '-=42',
+  }, '<')
 }
 
 const movable = [ '#deLorean', '#sideViewMirrorRight div:nth-child(2)' ]
@@ -44,4 +153,4 @@ const setModel = () => {
   setAddOn('#translateZ1', 'change', e => { moveModel('translate', 'Z', 1, e.target.value) })
 }
 
-export { setFuture, setModel }
+export { setFuture, setModel, beginFuture }
--- src/js/scenes/scene-13.js
+++ src/js/scenes/scene-13.js
@@ -1,17 +1,12 @@
-import { gsap, TimelineMax as TL } from 'gsap'
-
 import g from '../glob'
-import { toggleFermata } from '../utils'
+import { beginFuture } from '../future'
 
 const scene13 = 'Enter the DeLorean'
 
 const setScene13 = (c, n) => {
   g.scene.setting = c
 
-  g.tL.bttf = new TL({ defaults: { overwrite: 'auto' } })
-
-
-  spinTime()
+  beginFuture()
 
   return true
 }
@@ -40,91 +35,7 @@ const spinTime = () => {
   //   yoyo: true,
   // })
 
-  gsap.to('.lightBar > div', {
-    duration: 1.5,
-    ease: 'power1.in',
-    opacity: 0.12,
-    repeat: -1,
-    yoyo: true,
-  })
 
-  g.tL.bttf.to('#deLorean', {
-    duration: 3,
-    ease: 'power2.in',
-    onComplete: function () {
-      toggleFermata(true, { exceptTLs: [ 'bttf' ] })
-    },
-    opacity: 1,
-  })
-    .to('#deLorean', {
-      duration: 9,
-      ease: 'power2.in',
-      translateZ: -500,
-    })
-    .to('#deLorean', {
-      duration: 7.5,
-      ease: 'power2.in',
-      rotateY: '-=90',
-    }, '<5')
-    .to('#deLorean', {
-      duration: 3.5,
-      ease: 'power2.in',
-      rotateZ: '+=30',
-      repeat: 1,
-      yoyo: true,
-    }, '<2')
-    .to('#deLorean', {
-      duration: 5,
-      ease: 'power2.in',
-      translateX: '-=50',
-      translateY: '-=200',
-    }, '<')
-    .to('#deLorean', {
-      duration: 3.5,
-      ease: 'power2.in',
-      rotateX: '-=15',
-      repeat: 1,
-      yoyo: true,
-    }, '>')
-    .to('#deLorean', {
-      duration: 4,
-      ease: 'power2.out',
-      translateX: '+=150',
-      translateY: '-=100',
-      translateZ: -400,
-    }, '<')
-
-
-  // // CONVERT FROM FLYING TO DRIVING MODE
-  // //TURN OFF WHEEL ROCKETS
-  // gsap.to('#deLorean #wheels .wheel .rocket', {
-  //   delay: 5,
-  //   duration: 1.25,
-  //   scaleY: 0,
-  // })
-  // // SET WHEELS IN WELLS
-  // gsap.to('#wheelsLeft', {
-  //   duration: 4,
-  //   rotateY: 90,
-  //   translateZ: '-=42',
-  //   // repeat: -1,
-  //   // yoyo: true,
-  // })
-  // gsap.to('#wheelsRight', {
-  //   duration: 4,
-  //   rotateY: -90,
-  //   translateZ: '-=42',
-  //   // repeat: -1,
-  //   // yoyo: true,
-  // })
-
-  // gsap.to('#deLorean', {
-  //   duration: 30,
-  //   ease: 'none',
-  //   repeat: -1,
-  //   rotateZ: 360,
-  //   yoyo: true,
-  // })
 }
 
 export { scene13, setScene13 }
