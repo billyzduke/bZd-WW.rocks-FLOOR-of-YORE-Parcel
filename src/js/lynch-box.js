08a5d08bcf27206341d4626c9d8e5ac5780a192a
Billy Z Duke <billyzduke@gmail.com>
2021-06-10 10:56:00 -0700

    success with moving thru tunnel animation, currently based opposite mouse position, needs to be moved to lynchbox component and based opposite dragbox position...

--- src/js/lynch-box.js
+++ src/js/lynch-box.js
@@ -6,55 +6,38 @@ import { randOcolor } from './utils'
 const setLynchBox = () => {
   g.lynchBox = {
     bxs: [],
-    svg: SVG().addTo( '#mainStage' ).size( g.main.w, g.main.h ),
+    svg: SVG().addTo( '#mainStage' ).size( g.main.w, g.main.h ).css( { position: 'absolute', 'z-index': 1000 } ),
+    sts: {},
   }
-  g.lynchBox.svg.css( { position: 'absolute', 'z-index': 1000 } )
-  // console.log(g.lynchBox.svg.attr())
-  for ( let box = 0; box < 16; box++ ) g.lynchBox.bxs.push( drawBox() )
-  for ( let box = g.lynchBox.bxs.length - 1; box >= 0; box-- ) zoomBox( box )
+  for ( let bx = 0; bx < 16; bx++ ) g.lynchBox.bxs.push( drawBox() )
+  for ( let bx = g.lynchBox.bxs.length - 1; bx >= 0; bx-- ) zoomBox( bx )
 }
 
-const drawBox = () => {
-  const box = g.lynchBox.svg.rect( g.main.w / 10, g.main.h / 10 ).attr( {
-    fill: randOcolor(),
-    'fill-opacity': 0.1,
-  } )
-  translateBox( box )
-  return box
-}
+const drawBox = () => ( g.lynchBox.svg.rect( g.main.w / 10, g.main.h / 10 ).attr( {
+  fill: randOcolor(),
+  'fill-opacity': 0.1,
+} ) )
 
-const translateBox = ( box, pos ) => {
-  const bCX = g.main.cx - ( box.width() / 2 )
-  const bCY = g.main.cy - ( box.height() / 2 )
-  const bX = typeof g.m.x !== 'undefined' ? ( bCX * 2 ) - ( ( g.m.x + ( box.width() * ( g.m.x / g.main.w ) ) ) * ( 1 - pos ) ) : bCX
-  const bY = typeof g.m.y !== 'undefined' ? ( bCY * 2 ) - ( ( g.m.y + ( box.height() * ( g.m.y / g.main.h ) ) ) * ( 1 - pos ) ) : bCY
+const translateBox = ( bx, p ) => {
+  const box = g.lynchBox.bxs[bx]
+  let bX = g.main.cx - ( box.width() / 2 )
+  let bY = g.main.cy - ( box.height() / 2 )
+  if ( typeof g.m.x !== 'undefined' && typeof g.m.y !== 'undefined' ) {
+    bX -= ( ( g.m.x / g.main.scale ) - g.main.cx ) * ( 1 - p )
+    bY -= ( ( g.m.y / g.main.scale ) - g.main.cy ) * ( 1 - p )
+  }
   box.move( bX, bY )
 }
 
-const zoomBox = box => {
+const zoomBox = bx => {
   console.log()
-  g.lynchBox.bxs[box].animate( { duration: 7500, delay: 500 * box } ).ease( '<' )
+  g.lynchBox.bxs[bx].animate( { duration: 7500, delay: 500 * bx } ).ease( '<' )
     // .attr( { 'fill-opacity': 1 } )
     .size( g.main.w, g.main.h )
     .during( ( p, m, e ) => {
-      translateBox( g.lynchBox.bxs[box], p )
+      translateBox( bx, p )
     } )
     .loop()
 }
-
-// const zoomBoxes = () => {
-//   g.lynchBox.bxs.forEach( box => {
-//     zoomBox( box )
-//   } )
-// }
-
-// const animateLynchBox = () => {
-//   g.lynchBox.ntrvl = setInterval( () => {
-//     zoomBoxes()
-//     // if ( ++x === times ) {
-//     //   clearInterval( intervalID )
-//     // }
-//   }, 76 )
-// }
 
 export { /* animateLynchBox, */ setLynchBox }
