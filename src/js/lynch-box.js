002c7a5a2d9ab1c485c64e122bb4e4cb4be0a38b
Billy Z Duke <billyzduke@gmail.com>
2021-06-10 17:25:49 -0700

    aaaand just like that, bye-bye svg.js. perhaps again. it did a fine job of animating single rectangles, but anything more complex, even a super simple combo of two paths in an svg group, and it chokes hard. let's try pixi again I guess

--- src/js/lynch-box.js
+++ src/js/lynch-box.js
@@ -4,19 +4,17 @@ import g from './glob'
 import { randOcolor } from './utils'
 
 const setLynchBox = () => {
+  g.el.lynchBox.style.zIndex = 50
+  g.el.lynchBox.style.opacity = 1
   g.lynchBox = {
     bxs: [],
-    svg: SVG().addTo( '#mainStage' ).size( g.main.w, g.main.h ).css( { position: 'absolute', 'z-index': 1000 } ),
-    sts: {},
+    svg: SVG().addTo( '#lynchBox' ).size( g.main.w, g.main.h ).css( { position: 'absolute', 'z-index': 1000, 'background-color': '#000000' } ),
   }
-  for ( let bx = 0; bx < 16; bx++ ) g.lynchBox.bxs.push( drawBox() )
+  for ( let bx = 0; bx < 15; bx++ ) g.lynchBox.bxs.push( drawBox( ) )
   for ( let bx = g.lynchBox.bxs.length - 1; bx >= 0; bx-- ) zoomBox( bx )
 }
 
-const drawBox = () => ( g.lynchBox.svg.rect( g.main.w / 10, g.main.h / 10 ).attr( {
-  fill: randOcolor(),
-  'fill-opacity': 0.1,
-} ) )
+const drawBox = ( test = false ) => ( test ? g.lynchBox.svg.rect( g.main.w, g.main.h ).attr( { fill: '#2f2b29', 'fill-opacity': 0.1 } ) : SVG( '<g><path fill="#2f2b29" d="M0,0v1115h1115V0H0z M613.2,613.2H501.8V501.8h111.5V613.2z"/></g>' ).size( g.main.w, g.main.h ).addTo( g.lynchBox.svg ) )
 
 const translateBox = ( bx, p ) => {
   const box = g.lynchBox.bxs[bx]
@@ -31,13 +29,13 @@ const translateBox = ( bx, p ) => {
 
 const zoomBox = bx => {
   console.log()
-  g.lynchBox.bxs[bx].animate( { duration: 7500, delay: 500 * bx } ).ease( '<' )
-    // .attr( { 'fill-opacity': 1 } )
-    .size( g.main.w, g.main.h )
+  g.lynchBox.bxs[bx].back().animate( { duration: 7500, delay: 500 * bx } ).ease( '<' )
+    .attr( { fill: '#836E66' } )
+    .size( g.main.w * 10, g.main.h * 10 )
     .during( ( p, m, e ) => {
       translateBox( bx, p )
     } )
     .loop()
 }
 
-export { /* animateLynchBox, */ setLynchBox }
+export { setLynchBox }
